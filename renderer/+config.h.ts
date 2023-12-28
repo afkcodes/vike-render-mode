857b1d24aefcbe9f1cafea2733270e40ff2baeee
Ashish Kumar <ashish.kmr@outlook.com>
2023-12-28 19:02:23 +0530

    fixed client rendering

--- renderer/+config.h.ts
+++ renderer/+config.h.ts
@@ -1,4 +1,4 @@
-import type { Config } from "vike/types";
+import type { Config, ConfigEnv } from "vike/types";
 
 // https://vike.dev/config
 export default {
@@ -7,10 +7,35 @@ export default {
   // !! WARNING !! Before doing so, read https://vike.dev/clientRouting */
 
   // See https://vike.dev/data-fetching
+  clientRouting: true,
+  hydrationCanBeAborted: true,
   passToClient: ["pageProps", "urlPathname"],
   meta: {
-    Page: {
-      env: { server: false, client: true }, // SPA for all pages
+    renderMode: {
+      env: { config: true },
+      effect({ configDefinedAt, configValue }) {
+        let env: ConfigEnv | undefined;
+        if (configValue == "HTML") env = { server: true };
+        if (configValue == "SPA") env = { client: true };
+        if (configValue == "SSR") env = { server: true, client: true };
+        if (!env)
+          throw new Error(
+            `${configDefinedAt} should be 'SSR', 'SPA', or 'HTML'`
+          );
+        return {
+          meta: {
+            Page: { env },
+          },
+        };
+      },
+    },
+    title: {
+      // Make the value of `title` available on both the server- and client-side
+      env: { server: true, client: true },
+    },
+    description: {
+      // Make the value of `description` available only on the server-side
+      env: { server: true, client: true },
     },
   },
 } satisfies Config;
